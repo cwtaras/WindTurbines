5a37f97afe42b48dd2cf72c8015fa8503ddc645a
bugra <45214804+cwtaras@users.noreply.github.com>
2022-03-11 00:46:04 +0300

    deleted comment lines

--- main.py
+++ main.py
@@ -13,10 +13,10 @@ if __name__ == '__main__':
     hcnt = 100
 
     # 1 week data
-    # winddir_abs_comparison(begin_date, end_date)
-    # power_curve(begin_date, end_date, 300)
-    # winddir_relative_avg_for_all_turbine(begin_date, end_date, 300)
-    # winddir_relative_avg_hours(begin_date, end_date)
-    # winddir_relative_avg(begin_date,end_date, 300)
+    winddir_abs_comparison(begin_date, end_date)
+    power_curve(begin_date, end_date, 300)
+    winddir_relative_avg_for_all_turbine(begin_date, end_date, 300)
+    winddir_relative_avg_hours(begin_date, end_date)
+    winddir_relative_avg(begin_date,end_date, 300)
     winddir_abs_vs_windspeed(begin_date, end_date)
     # hcnt_avg_run(begin_date, end_date, hcnt)
